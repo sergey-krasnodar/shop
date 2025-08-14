880e7e9f8b33dbeb74e7a9cdd04dd7003f5ca8c5
sergey-krasnodar <serega.ru71@mail.ru>
2025-08-14 20:06:43 +0300

    add header

--- gulpfile.js
+++ gulpfile.js
@@ -54,7 +54,7 @@ function images() {
 
 
 function styles() {
-  return src('app/scss/style.scss')
+  return src('app/scss/*.scss')
     .pipe(autoprefixer({
       overrideBrowserslist: ['last 10 versions']
     }))
@@ -80,7 +80,7 @@ function watching() {
       baseDir: 'app/'
     }
   });
-  watch(['app/scss/style.scss'],styles)
+  watch(['app/scss/*.scss'],styles)
   watch(['app/images/src'], images)
   watch(['app/images/sprite'], sprites)
   watch(['app/pages/*', 'app/components/*'], pages)
